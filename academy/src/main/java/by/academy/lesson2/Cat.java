ab3a8bd3461b01b0805650d43f9201ecd783bd6e
OlegZhevnyak <ffieryice@gmail.com>
2022-06-14 21:25:01 +0300

    first commit

---
+++ academy/src/main/java/by/academy/lesson2/Cat.java
@@ -0,0 +1,33 @@
+package by.academy.lesson2;
+
+public class Cat {
+	int age;
+	String nickname;
+double money;
+	public void grow() {
+		age = age + 1;
+	}
+
+	public void sleep() {
+		System.out.println("Кот спит");
+	}
+
+	public void eat() {
+		System.out.println("Кот ест");
+	}
+
+	public void walk() {
+		System.out.println("Кот гуляет");
+	}
+
+	public Cat() {
+
+	}
+
+	public Cat(String nickname) {
+		this.nickname = nickname;
+	}
+	public setMoney(money) {
+		
+	}
+}
