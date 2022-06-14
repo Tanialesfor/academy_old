2869e9feaf19efb5c82ea9cf44036f5ed476ecb7
Tanialesfor <>
2022-06-14 22:01:35 +0300

    test

--- academy/src/main/java/by/academy/lesson2/Application.java
+++ academy/src/main/java/by/academy/lesson2/Application.java
@@ -1,20 +1,27 @@
 package by.academy.lesson2;
+
 public class Application {
 	public static void main(String[] args) {
 		Cat cat1 = new Cat();
 		Cat cat2 = new Cat("Пушок");
 		Cat cat3 = new Cat("");
 	}
+
 	public void grow(cat2) {
 	}
+
 	public void eat(cat2) {
 	}
+
 	public void walk(cat2) {
 	}
+
 	public void grow(cat3) {
 	}
+
 	public void grow(cat3) {
 	}
+
 	public void grow(cat3) {
 	}
 }
