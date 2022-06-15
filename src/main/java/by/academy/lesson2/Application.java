3617ef24209cce4c76e1fa85e77aaac70959eee8
Виталий <@PREDATOR>
2022-06-15 23:33:29 +0300

    homework

---
+++ src/main/java/by/academy/lesson2/Application.java
@@ -0,0 +1,25 @@
+package main.java.by.academy.lesson2;
+
+public class Application {
+	public static void main(String[] args) {
+		Cat cat1 = new Cat();
+		Cat cat2 = new Cat("Пушок");
+
+		cat2.eat();
+		cat2.sleep();
+		cat2.walk();
+		
+		cat1.grow();
+		cat1.grow();
+		cat1.grow();
+		
+		System.out.println(cat1.age);
+		
+//		cat1.setNickname("Nick");
+//		cat1.setInitials();
+//		System.out.println(cat1.getInitials());
+//		cat1.setNickname("Alex");
+//		cat1.setInitials2(cat1.nickname);
+//		System.out.println(cat1.getInitials());
+	}
+}
---
+++ src/main/java/by/academy/lesson2/Cat.java
@@ -0,0 +1,55 @@
+package main.java.by.academy.lesson2;
+
+public class Cat {
+	int age;
+	String nickname;
+	double money;
+	char initials;
+	
+	public void grow() {
+		age = age + 1;
+	}
+
+	public void sleep() {
+		System.out.println("Кот спит");
+	}
+
+	public void eat() {
+		System.out.println("Кот ест");
+	}
+
+	public void walk() {
+		System.out.println("Кот гуляет");
+	}
+
+	public Cat() {
+	}
+
+	public Cat(String nickname) {
+		this.nickname = nickname;
+		this.initials=nickname.charAt(0);
+	}
+	
+	public void setMoney(double money) {
+ 		this.money=money;
+ 	}
+	
+ 	public double getMoney() {
+ 		return money;
+ 	}
+ 	public void setInitials() {
+ 		this.initials=nickname.charAt(0);
+ 	}
+ 	public void setInitials2(String nick) {
+ 		this.initials=nick.charAt(0);
+ 	}
+	
+ 	public char getInitials() {
+ 		return initials;
+ 	}
+ 	
+ 	public void setNickname(String nickname) {
+ 		this.nickname=nickname;
+ 	}
+ 	
+}
